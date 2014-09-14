1b74363bf4b9cb346be3d4ba42dde9c72b1ad82e
ac7 <ac7@users.noreply.github.com>
2014-09-13 22:20:11 -0500

    Better comma-separated lists

--- room.go
+++ room.go
@@ -11,9 +11,15 @@ func (r *room) describeTo(viewer viewer) {
 	viewer.println("-=[{ "+r.name+" }]=-", green)
 	viewer.println(" "+r.desc, none)
 	viewer.print(" Here: ", black)
+
+	printedAnything := false
 	for _, thing := range r.contents {
 		if thing.id() != viewer.id() {
-			viewer.print(thing.name()+", ", black)
+			if printedAnything {
+				viewer.print(", ", black)
+			}
+			viewer.print(thing.name(), black)
+			printedAnything = true
 		}
 	}
 	viewer.newline()
